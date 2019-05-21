2a77898bc49082e35b7713726214a2a2d9cb563f
Steven <gonghaima@hotmail.com>
2019-05-21 11:47:16 +1000

    loading mock data into app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import data from "./data";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const Post = ({ id, title, body }) => (
+  <div className="post">
+    <div className="post-body">
+      <h4>{title}</h4>
+      <p>{body}</p>
     </div>
-  );
-}
+  </div>
+);
+
+const App = () => (
+  <div className="App">
+    <h2>LazyLoad Demo</h2>
+    <div className="post-container">
+      {data.map(post => (
+        <Post key={post.id} {...post} />
+      ))}
+    </div>
+  </div>
+);
 
 export default App;
