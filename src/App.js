5f262418f2ec88110e6a33e03345d06b00c5ae87
Steven <gonghaima@hotmail.com>
2019-05-21 12:40:34 +1000

    lazyloading added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import React from "react";
-import logo from "./logo.svg";
 import data from "./data";
+import LazyLoad from "react-lazyload";
+
+const Loading = () => (
+  <div className="post loading">
+    <h5>Loading...</h5>
+  </div>
+);
 
 const Post = ({ id, title, body }) => (
   <div className="post">
@@ -16,7 +22,9 @@ const App = () => (
     <h2>LazyLoad Demo</h2>
     <div className="post-container">
       {data.map(post => (
-        <Post key={post.id} {...post} />
+        <LazyLoad key={post.id} placeholder={<Loading />}>
+          <Post key={post.id} {...post} />
+        </LazyLoad>
       ))}
     </div>
   </div>
